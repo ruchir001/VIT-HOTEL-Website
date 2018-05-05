02917a7162b55e1d6dc36794b7a17e1fd1e8ccf1
Ruchir Bhaskar <ruchirbhaskar@gmail.com>
2018-05-05 15:27:20 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+	<title>Home Page</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
+</head>
+
+<body>
+
+	<?php
+		include "index2.php";
+	?>
+	<div id="heading">
+		<h1>Contact Us</h1>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-12">
+			<p id="contactP">
+				Email Address: vithotel@gmail.com
+			</p>
+			<p id="contactP">
+				Phone Number: 0121-2522588
+			</p>
+			<p id="contactP">
+				Address: Vit University
+			</p>
+		</div>
+	</div>
+
+
+
+<p align="center">
+<strong> FOLLOW US ON: </strong><a href="https://facebook.com/VIT_HOSTEL">
+<img border="0" alt="facebook" src="image\facebook.jpeg" width="50" height="50">
+</a>
+
+
+<a href="https://www.twitter.com/VIT_HOSTEL">
+<img border="0" alt="twitter" src="image\twitter1.png" width="50" height=50">
+</a>
+</p>
+
+</body>
+<marquee behavior=alternate bgcolor="#7e0000"><b><i><font color="white">Devloped By :- Ruchir Bhaskar </i></b></font></marquee>
+
+</html>
