52d55bf30ea6233ea7313c9363518a9722aa6691
Ruchir Bhaskar <ruchirbhaskar@gmail.com>
2018-05-14 13:24:32 +0530

    Add files via upload

---
+++ accommodation.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+	<title>Accomodation</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
+</head>
+
+<body>
+	<?php
+		include "index4.php";
+	?>
+
+	<div id = "heading">
+		<h1>Accommodation</h1>
+	</div>
+
+	<div class="container-fluid">
+		<div class="row" >
+			<a href="suite1.php"><img src="image/myroom.jpg" id='imgleft'></a>
+			<div id=heading>
+				<h2>Suite</h2>
+			</div>
+			<p>VIT HOTEL, among the premier business hotel in Vellore the economic capital of Tamil Nadu is characterised by the traditionally hospitality located in the heart of the city.The ideal choice amongs hotels in Vellore for businessman and tourist. </p>
+		</div>
+		<div class="row">
+			<img src="image/standard room.jpg" id="imgright">
+			<div id=heading>
+				<h2>Super Deluxe</h2>
+			</div>
+			<p>VIT HOTEL, among the premier business hotel in Vellore the economic capital of Tamil Nadu is characterised by the traditionally hospitality located in the heart of the city.The ideal choice amongs hotels in Vellore for businessman and tourist. </p>
+			<div class="hr"></div>
+		</div>
+		<div class="row">
+			<img src="image/delux room.jpg" id="imgleft">
+			<div id=heading>
+				<h2>Deluxe</h2>
+			</div>
+			<p>VIT HOTEL, among the premier business hotel in Vellore the economic capital of Tamil Nadu is characterised by the traditionally hospitality located in the heart of the city.The ideal choice amongs hotels in Vellore for businessman and tourist. </p>
+			<div class="hr"></div>
+		</div>
+		<div class="row">
+			<a href="standard1.php"><img src="image/meh.jpeg" id="imgright"></a>
+			<div id=heading>
+				<h2>Standard</h2>
+			</div>
+			<p>VIT HOTEL, among the premier business hotel in Vellore the economic capital of Tamil Nadu is characterised by the traditionally hospitality located in the heart of the city.The ideal choice amongs hotels in Vellore for businessman and tourist. </p>
+			<div class="hr"></div>
+		</div>
+
+	</div>
+
+	<script src="js/bootstrap.min.js"></script>
+<marquee behavior=alternate bgcolor="#7e0000"><b><i><font color="white">Devloped By :- Ruchir Bhaskar</i></b></font></marquee>
+
+</body>
+</html>
