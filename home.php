27ef6763b685bdf2098972d0c91602285870d009
Ruchir Bhaskar <ruchirbhaskar@gmail.com>
2018-05-19 14:19:19 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+	<title>Home Page</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
+</head>
+
+<body>
+
+	<?php
+		include "index.php";
+	?>
+
+	<div id = "heading">
+		<h1>Home</h1>
+	</div>
+
+	<div class="container-fluid">
+		<div class =  "row">
+			<p>VIT HOTEL, among the premier business hotel in Vellore is characterised by the traditionally college located in the heart of the city.The ideal choice amongs hotels in Vellore for Buissnessman.</p>
+		</div>
+		<div class="row">
+			<p>VIT HOTEL offers facilities that no other Vellore hotels offer. Each room is elegantly well appointed and equipped with complete amenities including aircondition fully stock minibars,directIDD telephone,TV with 24 hour satelight entertainment and news.Upscale bathroom with hair dryers and cosmetic mirrors,seperate hanging closets.</p>
+		</div>
+		<div class="row">
+			<p>The only one Vellore hotels having all facilities a business center,beauty parlor,health club,gym,saloon, boutique,internet cafe,pastry shop,travel desk and money exchange the PARAS hotels given you full value for money.</p>
+		</div>
+
+	</div>
+	<script src="js/bootstrap.min.js"></script>
+
+</body>
+<marquee behavior=alternate bgcolor="#7e0000"><b><i><font color="white">Devloped By :- Ruchir Bhaskar </i></b></font></marquee>
+
+</html>
