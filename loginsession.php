446556d3f062e4831439475d606ed6a7d86e4705
Ruchir Bhaskar <ruchirbhaskar@gmail.com>
2018-05-24 14:27:20 +0530

    Add files via upload

---
+++ loginsession.php
@@ -0,0 +1,23 @@
+<?php
+	session_start();
+
+	$l=mysqli_connect("localhost","root","","hotel");
+
+	$username=$_POST['username'];
+	$password=$_POST['password'];
+	$q="select * from `user` where  `Userid`='".$username."' and  `Password`='".$password."'";
+	$res=mysqli_query($l, $q);
+	if(mysqli_num_rows($res))
+	{
+	  $_SESSION['username']=$username;
+		echo "<script>window.location='reservation.php';</script>";
+	}
+	else
+	{
+		$message="Incorrect username/password found!";
+		echo "<script type='text/javascript'>alert('$message');</script>";
+		echo "<script>window.location='login.php';</script>";
+		echo"<script>close()</script>";
+	}
+
+?>
